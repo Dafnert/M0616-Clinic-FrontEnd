f68cad34427cedcc5622723be5fc68083b7cf0cf
ferranbustos <ferribb06@gmail.com>
2026-04-27 16:05:49 +0200

    fix #4 agenda view

--- clinical/src/app/app.routes.ts
+++ clinical/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AgendaComponent } from './agenda/agenda.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'agenda', pathMatch: 'full' },
+  { path: 'agenda', component: AgendaComponent },
+];
